3463eaf94deff7b271a6b984c67fb29af261d589
Donatas Piragis <donatas.piragis@gmail.com>
2019-12-06 11:13:24 +0200

    Node Express Files deleted

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 9000
-//const morgan = require('morgan');
-//app.use(morgan('dev'));
-app.use(express.static('public'));
-app.get('*', (req, res) => res.sendFile(__dirname + '/'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
